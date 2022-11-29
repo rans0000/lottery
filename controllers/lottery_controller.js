8bf3af27ca54cb92a5e09a64ac477bdb50dffc4c
rans0000 <rans0000@gmail.com>
2022-11-30 00:18:09 +0530

    add method to parse pdf file in lottery controller

--- controllers/lottery_controller.js
+++ controllers/lottery_controller.js
@@ -1,37 +1,49 @@
 import { PdfReader } from 'pdfreader';
 import LotteryResult from '../classes/lottery_result';
 
-const parseLotteryBuffer = buffer => {
+const parsePdf = function (resolve, reject) {
     let data = '';
     let lotteryResult = new LotteryResult();
     const is4Lettered = true;
 
+    return (error, item) => {
+        if (error) {
+            console.error("error at pdf parsing:", error);
+            reject(error);
+        }
+        else if (!item) {
+            /** End of buffer */
+            resolve({ ...lotteryResult.get() });
+        }
+        else if (item.text) {
+            data += item.text;
+            lotteryResult
+                .dateParser(item.text)
+                .identifier('1st Prize Rs', 'prize1', item.text)
+                .identifier('Cons Prize-Rs', 'prizeCons', item.text)
+                .identifier('2nd Prize Rs', 'prize2', item.text)
+                .identifier('3rd Prize Rs', 'prize3', item.text)
+                .identifier('4th Prize-Rs', 'prize4', item.text, is4Lettered)
+                .identifier('5th Prize-Rs', 'prize5', item.text, is4Lettered)
+                .identifier('6th Prize-Rs', 'prize6', item.text, is4Lettered)
+                .identifier('7th Prize-Rs', 'prize7', item.text, is4Lettered)
+                .identifier('8th Prize-Rs', 'prize8', item.text, is4Lettered)
+                .append(item.text);
+        }
+    };
+};
+
+/** @param {String} filename with path ex: 'test/sample.pdf' */
+const parseLotteryFile = file => {
     const promise = new Promise((resolve, reject) => {
-        new PdfReader().parseBuffer(buffer, (error, item) => {
-            if (error) {
-                console.error("error at pdf parsing:", error);
-                reject(error);
-            }
-            else if (!item) {
-                /** End of buffer */
-                resolve({ ...lotteryResult.get() });
-            }
-            else if (item.text) {
-                data += item.text;
-                lotteryResult
-                    .dateParser(item.text)
-                    .identifier('1st Prize Rs', 'prize1', item.text)
-                    .identifier('Cons Prize-Rs', 'prizeCons', item.text)
-                    .identifier('2nd Prize Rs', 'prize2', item.text)
-                    .identifier('3rd Prize Rs', 'prize3', item.text)
-                    .identifier('4th Prize-Rs', 'prize4', item.text, is4Lettered)
-                    .identifier('5th Prize-Rs', 'prize5', item.text, is4Lettered)
-                    .identifier('6th Prize-Rs', 'prize6', item.text, is4Lettered)
-                    .identifier('7th Prize-Rs', 'prize7', item.text, is4Lettered)
-                    .identifier('8th Prize-Rs', 'prize8', item.text, is4Lettered)
-                    .append(item.text);
-            }
-        });
+        new PdfReader().parseFileItems(file, parsePdf(resolve, reject));
+    });
+    return promise;
+};
+
+const parseLotteryBuffer = buffer => {
+    const promise = new Promise((resolve, reject) => {
+        new PdfReader().parseBuffer(buffer, parsePdf(resolve, reject));
     });
     return promise;
 };
@@ -83,4 +95,4 @@ const searchForPrize = async (ticketNo, date, db) => {
     return promise;
 };
 
-export { parseLotteryBuffer, saveLotteryResultToDB, searchForPrize };
+export { parseLotteryFile, parseLotteryBuffer, saveLotteryResultToDB, searchForPrize };
