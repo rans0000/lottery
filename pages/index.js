563134808f45e8d590ce3f0967cc678f437e59c9
rans0000 <rans0000@gmail.com>
2022-11-29 23:52:06 +0530

    add tab for switching between prize & result search widgets

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,53 @@
+import React from 'react';
 import Head from 'next/head';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
-import styles from '../styles/Home.module.css';
 import LotteryNumberSearchForm from '../components/LotteryNumberSearchForm';
 
 export default function Home() {
+  const [currentTab, setCurrentTab] = React.useState(0);
+
+  const onTabChange = (event, value) => {
+    setCurrentTab(value);
+  };
+
   return (
     <div>
       <Head>
         <title>Lottery Website | Home</title>
       </Head>
-
+      <Tabs value={currentTab} onChange={onTabChange}>
+        <Tab label='Prize Search' />
+        <Tab label='Result Search' />
+      </Tabs>
+      <TabPanel value={currentTab} index={0}>
+        <LotteryNumberSearchForm />
+      </TabPanel>
+      <TabPanel value={currentTab} index={1}>
+        results page...
+      </TabPanel>
       <div>
         <br />
-        <LotteryNumberSearchForm />
       </div>
     </div>
   );
 }
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return value === index ? (
+    <Box
+      sx={{ p: 3 }}
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {children}
+    </Box>) : null;
+}
+
